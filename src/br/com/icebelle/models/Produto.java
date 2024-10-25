a8a0cc9c6f6ee57057b06c8936a9295a7b691320
Rubens Gomes <gomez.rubens@live.com>
2024-10-24 22:40:21 -0300

    Adicionando funcionalidades de cadastro de produtos (ainda em customização).

--- src/br/com/icebelle/models/Produto.java
+++ src/br/com/icebelle/models/Produto.java
@@ -19,12 +19,4 @@ public class Produto {
     public String getNome() {
         return nome;
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
