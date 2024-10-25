65646be7ff28667256b49483622542d1e687f943
Rubens Gomes <gomez.rubens@live.com>
2024-10-24 22:39:42 -0300

    Adicionando funcionalidades de cadastro de produtos (ainda em customização).

--- src/br/com/icebelle/views/Auth.java
+++ src/br/com/icebelle/views/Auth.java
@@ -3,10 +3,10 @@ package br.com.icebelle.views;
 import java.util.Scanner;
 
 public class Auth {
-    private Messages messages = new Messages();
-    private Scanner scanner = new Scanner(System.in);
-
     public Auth() {
+        Messages messages = new Messages();
+        Scanner scanner = new Scanner(System.in);
+
         messages.setWarning("\n[*] Tela de autenticação\n\n");
         messages.setWarning("Login: ");
         scanner.next();
